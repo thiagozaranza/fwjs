d6737b9398c2fcd4f51a3344675cd6e1d0968bb1
Thiago Zaranza <thiagozaranza@gmail.com>
2017-11-16 10:12:32 -0300

    Put pagination after table in Grid component

--- components/grid.js
+++ components/grid.js
@@ -558,7 +558,7 @@ FW.components.Grid = function(domr, controller) {
         if (!paginator.total)
             return;
 
-        Grid.table.parent().append(
+        Grid.table.after(
               '<div class="row paginator">'
             + '     <div class="col-xs-2 col-sm-2 col-md-2 col-lg-1 paginator-info" style="margin-top: 15px; padding-right: 0px"></div>'
             + '     <div class="col-xs-8 col-sm-8 col-md-8 col-lg-10" style="" >'
